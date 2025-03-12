94e8db124c74d23a338925d94933d896af1eb6da
Schneider <schneidercadet@live.fr>
2025-03-12 17:03:02 -0400

    Replace hardcoded values with JSON data and add skeleton UI

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import topSellersData from "../../api/topSellers.json";
+import Skeleton from "../UI/Skeleton";
+
+const TopSellerSkeleton = () => (
+  <li>
+    <div className="author_list_pp">
+      <Skeleton 
+        type="avatar" 
+        width="50px" 
+        height="50px" 
+        borderRadius="50%" 
+        style={{ margin: "0 auto" }}
+      />
+    </div>
+    <div className="author_list_info">
+      <Skeleton 
+        type="title" 
+        width="80px" 
+        height="16px" 
+        style={{ marginBottom: "8px" }}
+      />
+      <Skeleton 
+        type="subtitle" 
+        width="60px" 
+        height="14px"
+      />
+    </div>
+  </li>
+);
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTopSellers = async () => {
+      try {
+        setTopSellers(topSellersData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error loading top sellers data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchTopSellers();
+  }, []);
+
+  const renderSkeletonItems = () => {
+    return Array(12).fill(0).map((_, index) => (
+      <TopSellerSkeleton key={`skeleton-${index}`} />
+    ));
+  };
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +67,28 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading ? (
+                renderSkeletonItems()
+              ) : (
+                topSellers.map((seller) => (
+                  <li key={seller.id}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${seller.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={seller.authorImage || AuthorImage}
+                          alt={seller.authorName}
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${seller.authorId}`}>{seller.authorName}</Link>
+                      <span>{seller.price} ETH</span>
+                    </div>
+                  </li>
+                ))
+              )}
             </ol>
           </div>
         </div>
